4a2d33755c374f52fd20cb8bfbdaf405be64854f
Luizfernando791 <luizfernando.macera@gmail.com>
2025-02-26 10:26:24 -0300

    Update Atividade1.java

--- Java/src/Aula3desafio1/src/Aula3desafio1/Atividade1.java
+++ Java/src/Aula3desafio1/src/Aula3desafio1/Atividade1.java
@@ -1,9 +1,37 @@
 package Aula3desafio1;
 
+import java.util.Scanner;
+
 public class Atividade1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			Scanner myObj = new Scanner(System.in); 
+			
+			System.out.println("digite a primeira nota " );
+		    double n1 =  myObj.nextDouble() ;  
+	
+		    System.out.println("digite a segunda nota: " );
+		    double n2 =  myObj.nextDouble() ;  
+	
+		    System.out.println("digite a terceira nota ");
+		    double n3 =  myObj.nextDouble() ; 	
+		    
+		    double resultado = (n1 + n2 + n3) / 3;
+		    if (resultado < 5) {
+		    	System.out.println("reprovado ");
+			}
+		    else if (resultado >= 5 ) {
+				if (resultado <=6.9) {
+					System.out.println("recuperação ");
+				}
+			}else if (resultado >= 7 ) {
+				if (resultado <= 8.9) {
+					System.out.println("aprovado");
+				}
+			}
+			else {
+				System.out.println("aprovado com distição - excelente!");
+			}
 
 	}
 
