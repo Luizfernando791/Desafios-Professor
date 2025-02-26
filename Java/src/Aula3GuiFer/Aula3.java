72609cd17e7cd6da8f6378641e55d1db0eaed361
BinnBingus <bielblacksouza@gmail.com>
2025-02-26 08:27:22 -0300

    Codigo1

--- Java/src/Aula3GuiFer/Aula3.java
+++ Java/src/Aula3GuiFer/Aula3.java
@@ -1,25 +1,10 @@
 
 package Aula3GuiFer;
-import java.util.Scanner;
 
 public class Aula3 {
 	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		
-		double Celsius;
-		double Fah;
-		double Kel;
-		
-		System.out.print("Graus em Celsius para a conversão em Fahrenheit e Kelvin  : ");
-		Celsius = sc.nextDouble();
-		
-		Fah = (Celsius * 9/5) + 32;
-		Kel = (Celsius + 273.15);
-		
-		System.out.print("Fahrenheits : " + Fah + "   Kelvin : " + Kel);
-		
-		sc.close();												
 	}
 
 }
